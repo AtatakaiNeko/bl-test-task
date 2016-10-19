5db2a177246041dae0c677acbbea1846d57d8c0a
Mykhailo Zvonov <mykhailo.zvonov@accenture.com>
2016-10-19 21:35:25 +0300

    Login page
    Profile page
    Spring animation

---
+++ app/src/main/java/com/inspired/ppc/login/LoginFragment.java
@@ -0,0 +1,10 @@
+package com.inspired.ppc.login;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mykhailo.zvonov on 19/10/2016.
+ */
+
+public class LoginFragment extends Fragment {
+}
